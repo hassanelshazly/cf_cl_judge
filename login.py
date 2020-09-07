4835a9764797902e7fd3927e9d3bcb04cd5ec0a3
Hassan El-shazly <hassanelshazly2010@gmail.com>
2020-09-07 07:51:12 +0200

    login as a function

--- login.py
+++ login.py
@@ -1,18 +1,11 @@
 from bs4 import BeautifulSoup as bs
-from params import *
 import requests
 import getpass
+import params
+import util
 import json
 import time
 import re
-
-def get_csrf_token(html):
-    soup = bs(html, features="lxml")
-    head = soup.head
-    meta = head.findChildren("meta")
-    csrf_token = [
-        m for m in meta if "name" in m.attrs and m["name"] == "X-Csrf-Token"]
-    return csrf_token[0]["content"]
 
 def check_login_error(html):
     soup = bs(html, features="lxml")
@@ -20,41 +13,39 @@ def check_login_error(html):
     if len(error) > 0:
         return error[0].text
 
+def login(client):
+    cf_enter = "{base}/enter".format(base=params.base)
+    login_page = client.get(cf_enter)
+    csrf_token = util.get_csrf_token(login_page.content)
+    # print (csrf_token)
 
-cf_enter = "{base}/{login}".format(base=base, login=login_url)
+    login_data = {
+        "csrf_token"   : csrf_token,
+        "handleOrEmail": params.user,
+        "password"     : params.password,
+        "ftaa"         : params.ftaa,
+        "bfaa"         : params.bfaa,
+        "_tta"         : params.tta,
+        "action"       : "enter",
+        "remember"     : "on"
+    }
 
-cliente = requests.session()
-login_page = cliente.get(cf_enter)
-csrf_token = get_csrf_token(login_page.content)
-
-# print (csrf_token)
-print(tta)
-user = input("Handle/Email: ")
-if not len(user):
-    user = defind_user
-
-password = getpass.getpass("Password: ")
-if not len(password):
-    user = defind_password
-
-login_data = {
-    "csrf_token"   : csrf_token,
-    "action"       : "enter",
-    "handleOrEmail": user,
-    "password"     : password,
-    "remember"     : "on",
-    "ftaa"         : ftaa,
-    "bfaa"         : bfaa,
-    "_tta"         : tta
-}
-
-login_result = cliente.post(cf_enter, data=login_data)
-# print (login_result.status_code)
-if login_result.status_code != 200:
-    print ("fail to connect")
-else:
-    error = check_login_error(login_result.content)
-    if error == None:
-        print("Successful login")
+    login_result = client.post(cf_enter, data=login_data)
+    # print (login_result.status_code)
+    if login_result.status_code != 200:
+        print ("fail to connect", cf_enter)
+        return False
     else:
-        print(error)
+        error = check_login_error(login_result.content)
+        if error == None:
+            print("Successful login")
+        else:
+            print(error)
+            return False
+    return True
+
+if __name__ == "__main__":
+    client = requests.session()
+    login(client)
+    import submit
+    submit.submit(client)
