a3af087ceeb9fd55903956817be6ebb5b43d09e7
Hassan El-shazly <hassanelshazly2010@gmail.com>
2021-01-18 21:07:09 +0200

    Delete old code

--- util.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup as bs
-
-def get_csrf_token(html):
-    soup = bs(html, features="lxml")
-    head = soup.head
-    meta = head.findChildren("meta")
-    csrf_token = [
-        m for m in meta if "name" in m.attrs and m["name"] == "X-Csrf-Token"]
-    return csrf_token[0]["content"]
